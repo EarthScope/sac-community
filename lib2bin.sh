9e2556067e5c7ff3d35e30d0362cb25d72ff8236
Brian Savage <savage@uri.edu>
2017-07-28 16:17:53 -0400

    Added libsac_all and program to generate c files from static libs

---
+++ lib2bin.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+LIB=$1
+SRC=$2
+
+FUNCS=$(nm $LIB  | grep ' T ' | awk '{print $3}' |  sed 's/^_//' )
+
+for f in $FUNCS; do
+    echo "void $f();"
+done
+
+cat << EOF
+void not_used(int v);
+int main() {
+   not_used(0);
+   return 0;
+}
+void
+not_used(int v) {
+   if (v == 0) {
+       return;
+   }
+EOF
+for f in $FUNCS; do
+    echo "     $f();"
+done
+
+#    awk '{printf("void %s(); %s();\n", $1, $1)}' 
+
+cat << EOF
+}
+EOF
