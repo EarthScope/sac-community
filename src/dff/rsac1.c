e136d93b094b64912e9fb74d1f5a621d77a36d70
Brian Savage <savage@uri.edu>
2018-02-20 14:34:45 -0500

    Changed check precison from unsafe casting to nextafterf()

--- src/dff/rsac1.c
+++ src/dff/rsac1.c
@@ -207,11 +207,7 @@ sac_data_read_new(sac *s, FILE *fp) {
 
 double
 check_precision(float dt, float val) {
-    float df;
-    float f2 = val;
-    int *i = (int *) &f2; // Cast float memory to integer value
-    *i = *i + 1;          // Increment to the next float
-    df = f2 - val;        // Find difference between neighboring floats
+    float df = val - nextafterf(val,0.0);
     if(df > dt) {         // Return difference if larger than sampling rate
         return df;
     }
