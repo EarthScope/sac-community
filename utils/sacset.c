e5665e109c25d81dc0461b21e0985dde03ac57a1
Brian Savage <savage@uri.edu>
2019-09-30 18:58:23 -0400

    Initial version of utils/sacset

---
+++ utils/sacset.c
@@ -0,0 +1,174 @@
+
+#include <stdio.h>
+#include <sacio.h>
+#include <errno.h>
+#include <float.h>
+#include <limits.h>
+#include <string.h>
+
+
+int
+bool_type(char *val) {
+    char *on[]  = {"1", "true", "on", "yes"};
+    char *off[] = {"0", "false", "off", "no"};
+    for(int k = 0; k < 4; k++) {
+        if(strcasecmp(val, on[k]) == 0) {
+            return 1;
+        }
+    }
+    for(int k = 0; k < 4; k++) {
+        if(strcasecmp(val, off[k]) == 0) {
+            return 0;
+        }
+    }
+    return -1;
+}
+
+int
+main(int argc, char *argv[]) {
+    int nerr = 0;
+    int i = 0, j = 0;
+    char *key = NULL;
+    char *val = NULL;
+    sac *s = NULL;
+    double dv = 0.0;
+    int iv = 0;
+    int verbose = 0;
+    char *endptr = NULL;
+    char arg[2048] = {0};
+    struct hid *h = NULL;
+    struct eid *e = NULL;
+    int files = 0;
+    timespec64 t = {0,0};
+    timespec64 t0 = {0,0};
+    
+    for(i = 1; i < argc; i++) {
+        if(strcmp(argv[i], "-v") == 0 ||
+           strcmp(argv[i], "--verbose") == 0) {
+            verbose = 1;
+        } else if(argv[i][0] != '-') {
+            files++;
+        }
+    }
+    if(files == 0) {
+        printf("Error, no files specified, exiting\n");
+        exit(-1);
+    }
+
+    for(j = 1; j < argc; j++) {
+        if(argv[j][0] == '-') {
+            continue;
+        }
+        if((s = sac_read_header(argv[j], &nerr)) == NULL) {
+            printf("File not found, skipping: %s\n", argv[j]);
+            continue;
+        }
+        if(verbose) {
+            printf("Working on file: %s\n", argv[j]);
+        }
+        for(i = 1; i < argc; i ++) {
+            /* Only allow dashed options from here */
+            if(argv[i][0] != '-') { 
+                continue;
+            }
+            /* Skip verbose option */
+            if(strcmp(argv[i], "-v") == 0 || strcmp(argv[i], "--verbose") == 0) {
+                continue;
+            }
+            /* Parse --key=val */
+            key = val = NULL;
+            strcpy(arg, argv[i]);
+            key = arg;
+            /* Remove leading --- */
+            while(*key == '-') {
+                key++;
+            }
+            /* Find equal sign, splitting option */
+            if((val = strchr(arg, '=')) == NULL) {
+                printf("\tWarning, expected -keyword=value, found %s\n", arg);
+                continue;
+            }
+            *val = 0; /* Set equal to terminator, null-terminating key string */
+            val++; /* Set val to first character in value */
+            if(!*key || !*val) {
+                val--;
+                *val = '=';
+                printf("\tWarning, expected -keyword=value, found %s\n", arg);
+                continue;
+            }
+
+            /* Determine the Header ID */
+            if((h = sac_keyword_to_header(key, strlen(key))) == NULL) {
+                printf("Unrecognized keyword, skipping: %s\n", key);
+                continue;
+            }
+            /* Set the header value to 'val' */
+            switch(h->type) {
+            case SAC_FLOAT_TYPE:
+                if(sac_is_timeval(h->id) && timespec64_parse(val, &t)) {
+                    if(! sac_get_time_ref(s, &t0)) {
+                        printf("error setting time for %s = %s (no reference time set)\n", key, val);
+                        continue;
+                    }
+                    int64_t sec  = t.tv_sec - t0.tv_sec;
+                    int64_t nsec = t.tv_nsec - t0.tv_nsec;
+                    double dt = (double) sec + (double) nsec / (double) 1000000000;
+                    sac_set_float(s, h->id, dt);
+                    break;
+                }
+                dv = strtod(val, &endptr);
+                if(endptr == NULL || endptr == val || errno == ERANGE || errno == EINVAL || *endptr != 0) {
+                    printf("error converting argument to number: %s\n", val);
+                    continue;
+                }
+                sac_set_float(s, h->id, dv);
+                break;
+            case SAC_INT_TYPE:
+                iv = strtol(val, &endptr, 10);
+                if(endptr == val || errno == ERANGE || errno == EINVAL) {
+                    printf("error converting argument to integer: %s\n", val);
+                    continue;
+                }
+                sac_set_int(s, h->id, iv);
+                break;
+            case SAC_ENUM_TYPE:
+                e = sac_enum_to_id(val, strlen(val));
+                if(!e) {
+                    printf("error, unknown enum type: %s\n", val);
+                    continue;
+                }
+                sac_set_int(s, h->id, e->id);
+                break;
+            case SAC_BOOL_TYPE:
+                if((iv = bool_type(val)) == -1) {
+                    printf("error, unknown logical type: %s\n", val);
+                    continue;
+                }
+                sac_set_int(s, h->id, iv);
+                break;
+            case SAC_STRING_TYPE:
+            case SAC_LONG_STRING_TYPE:
+                sac_set_string(s, h->id, val);
+                break;
+            case SAC_AUX_TYPE:
+                if(h->id == SAC_DATE_TIME) {
+                    if(!timespec64_parse(val, &t)) {
+                        printf("error parsing date-time: %s\n", val);
+                        continue;
+                    }
+                    sac_set_time(s, t);
+                    break;
+                }
+            default:
+                printf("Unimplemented keyword, skipping: %s %d %d\n", key, h->type, h->id);
+                continue;
+            }
+            if(verbose) {
+                printf("\t%-12s %s\n", key, val);
+            }
+        }
+        sac_write_header(s, argv[j], &nerr);
+        sac_free(s);
+        s = NULL;
+    }
+}
