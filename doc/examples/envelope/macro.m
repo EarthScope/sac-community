75b3f7f36867a688b792546b46490cdf770a7d71
Brian Savage <savage@uri.edu>
2019-12-02 09:30:09 -0500

    Added changes from JAS to doc/examples

--- doc/examples/envelope/macro.m
+++ doc/examples/envelope/macro.m
@@ -1,5 +1,7 @@
 echo on
 fg seismo
+write original.sac
+rtr ; taper
 write raw.sac
 envelope
 write env_sac.sac
--- doc/examples/taper/macro.m
+++ doc/examples/taper/macro.m
@@ -1,4 +1,12 @@
-fg random
+echo on
+fg seismo
+write original.sac
+r original.sac
+rtrend
+write raw.sac
+r raw.sac
+cut 10.45 10.6
+r
 write raw.sac
 taper
 write taper_sac.sac
