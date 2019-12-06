4dce6069d8ab54cbe4233ca9da6eff569eb24722
Brian Savage <savage@uri.edu>
2019-12-06 14:32:08 -0500

    Changed name of output in filter example

--- doc/examples/filter/filterf.f90
+++ doc/examples/filter/filterf.f90
@@ -7,9 +7,9 @@ subroutine test_bp()
   call rsac1("raw.sac", y, n, b, dt, nmax, nerr)
   call remove_trend(y, n, dt, b)
   call bandpass(y, n, dt, 0.10, 1.00)
-  call wsac0("bandpass.sac", y, y, nerr)
+  call wsac0("bandpassf.sac", y, y, nerr)
   if(sac_compare_to_file("bandpass_sac.sac", y, 1e-4, 0, 0) .ne. 0) then
-     write(*,*)'data does not match file'
+     write(*,*)'data does not match file for bandpass'
      call exit(1)
   endif
 end subroutine test_bp
@@ -22,9 +22,9 @@ subroutine test_lp()
   call rsac1("raw.sac", y, n, b, dt, nmax, nerr)
   call remove_trend(y, n, dt, b)
   call lowpass(y, n, dt, 2.0)
-  call wsac0("lowpass.sac", y, y, nerr)
+  call wsac0("lowpassf.sac", y, y, nerr)
   if(sac_compare_to_file("lowpass_sac.sac", y, 1e-4, 0, 0) .ne. 0) then
-     write(*,*)'data does not match file'
+     write(*,*)'data does not match file for lowpass'
      call exit(1)
   endif
 end subroutine test_lp
@@ -37,9 +37,9 @@ subroutine test_hp()
   call rsac1("raw.sac", y, n, b, dt, nmax, nerr)
   call remove_trend(y, n, dt, b)
   call highpass(y, n, dt, 10.0)
-  call wsac0("highpass.sac", y, y, nerr)
+  call wsac0("highpassf.sac", y, y, nerr)
   if(sac_compare_to_file("highpass_sac.sac", y, 1e-4, 0, 0) .ne. 0) then
-     write(*,*)'data does not match file'
+     write(*,*)'data does not match file for highpass'
      call exit(1)
   endif
 end subroutine test_hp
