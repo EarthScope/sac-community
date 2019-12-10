e80074d00c37b1eaba4996a1547fcec2c61b948c
Brian Savage <savage@uri.edu>
2019-12-10 11:25:10 -0500

    Update time_shift doc/example

--- doc/examples/time_shift/run_time_shift.sh
+++ doc/examples/time_shift/run_time_shift.sh
@@ -1,20 +1,23 @@
 #!/bin/sh
 
+\rm -f *.sac time_shift
+
 echo "Creating input for timeshift ..."
 sac --copyright-off <<EOF
 fg seismo
 rtr ; taper
 chnhdr allt (0 - &1,o&) IZTYPE IO
-write seismo.sac
 lp co 5.0 np 4
 ch kevnm "lp_co5_np4"
-write seismo_lpco5np4.sac
-m ${SACHOME}/macros/sac-ts.m seismo_lpco5np4.sac seismo_lpco5np4_sacts.sac -0.05
+write lpco5np4.sac
+read lpco5np4.sac
+rtr ; taper
+m ${SACHOME}/macros/sac-ts.m lpco5np4.sac lpco5np4_sacts.sac -0.05
 quit
 EOF
 
-INPUT=seismo_lpco5np4.sac
-OUTPUT=seismo_lpco5np4_ts.sac
+INPUT=lpco5np4.sac
+OUTPUT=lpco5np4_ts.sac
 
 echo "Compiling timeshift ..."
 gfortran -Wall -Wextra -fbounds-check -o time_shift time_shift.f  `sac-config --libs libsac libsacio`
@@ -22,9 +25,12 @@ gfortran -Wall -Wextra -fbounds-check -o time_shift time_shift.f  `sac-config --
 echo "Running timeshift ..."
 ./time_shift $INPUT $OUTPUT -0.05
 
-echo "Comparing results"
+echo "Comparing results this run and stored"
 for z in *.sac ; do
-    sacdiff -f 1e-4 $z sample-run/$z
+    sacdiff -qf 1e-4 $z sample-run/$z
 done
+
+echo "Comparing program and SAC time-shifts"
+sacdiff -qf 1e-4 lpco5np4_sacts.sac lpco5np4_ts.sac
 
 
